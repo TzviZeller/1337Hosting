987a3a39fe411fce73b49ea77779c8776596dc9c
M. Alex Boyd <mboyd8@students.towson.edu>
2016-12-31 18:22:24 -0500

    Added 404 Routing and Base64 Code
    Added correct 404 routing to 404.html when /gallery/##### is an invalid
    image.
    
    Added a textarea for base64 input for users if they want it. However,
    char length seems to be breaking text area/input so will need to figure
    out fix + add easy copy+paste.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
-import os,subprocess
+import os
+import subprocess
+
 app = Flask(__name__)
 
 
@@ -15,8 +17,8 @@ def genImage():
             # find the .txt file holding our incrementation mechanism
             incrementNum = f.read().splitlines()
             if incrementNum > 0:
-            # path to new image upload's template
-                writepath = '../../../../var/www/imageHot/imageHot/templates/gallery/' + str(incrementNum[0]) + ".html"
+                # path to new image upload's template
+                writepath =  '../../../../var/www/imageHot/imageHot/templates/gallery/' + str(incrementNum[0]) + ".html"
             else:
                 f.close()
         with open(writepath, "a+") as f:
@@ -25,10 +27,14 @@ def genImage():
             # Anyway, this will write to the .html file to create the newly uploaded image's page.
             htmlHeaders = "<!doctype html><html lang='en'><head><meta charset='utf-8'><title>1337Site</title><meta name='description' content='We host images lulz'></head><body>\n"
             styles = '<link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue-green.min.css" /><link rel="stylesheet" href="{{ url_for(\'static\', filename=\'style.css\')}}" /><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">    <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">"\n'
+            # javaScript = '<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script><script src="https://cdn.jsdelivr.net/clipboard.js/1.5.16/clipboard.min.js"></script><script src="{{ url_for(\'static\', filename=\'clippy.js\')}} "></script>\n'
             htmlFooter = "</body></html>"
             imageBox = "<div id=\"experienceBox\" class=\"mdl-cell mdl-cell--4-col mdl-shadow--2dp\">\n"
             linkBox = "<div id=\"linkBox\" class=\"mdl-cell mdl-cell--4-col mdl-shadow--2dp\">\n"
-            link = "<input id='link' type='text' value='http://104.131.183.72/gallery/" + str(incrementNum[0]) + "'/>"
+            link = "<input class='mdl-textfield__input' id='link' type='text' value='http://104.131.183.72/gallery/" + str(incrementNum[0]) + "'/>"
+            # linkLabel = "<label class='mdl-textfield__label' for='link'>Link </label>\n"
+            # link64Chars = "<textarea class='mdl-textfield__input' id='64bitImg' style='overflow:scroll;' type='text' rows='3' value=' " + str(imgCode) + "'/>"
+            # charLabel = "<label class='mdl-textfield__label' for='64bitImg'>Base64 </label>\n"
             f.write(htmlHeaders)
             f.write(styles)
             f.write(imageBox)
@@ -36,7 +42,13 @@ def genImage():
             f.write("</div>\n")
             f.write(linkBox)
             f.write(link)
+            # f.write(linkLabel)
             f.write("</div>\n")
+            # f.write(linkBox)
+            # f.write(link64Chars)
+            # f.write(charLabel)
+            # f.write("</div>\n")
+            # f.write(javaScript)
             f.write(htmlFooter)
 
             # After writing is over, run the bash script to clean up and increment...
@@ -57,9 +69,16 @@ def hello():
 # should 404 if doesn't exist, can implement better solution later.
 @app.route("/gallery/<int:imageId>")
 def galleryRoute(imageId):
-    imageURL = "/gallery/" + str(imageId) + ".html"
-    return render_template(imageURL);
+    list1 = list()
+    imageURL = "/var/www/imageHot/imageHot/templates/gallery/" + str(imageId) + ".html"
+    if os.path.isfile(imageURL):
+        return render_template("/gallery/" + str(imageId) + ".html")
+    else:
+        return render_template('404.html', file1="A sly fox stole the image, sorry!")
 
+
+def getRandomNum():
+    return 4
 
 if __name__ == "__main__":
     app.config.update(
